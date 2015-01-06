6e56ada57e6627c722299308d15ebeadfe6f5020
Marc Reichelt <mcreichelt@gmail.com>
2015-01-06 17:45:09 +0100

    added bsdiff and vcdiff (so far vcdiff is best)

--- initial_test/create_diffs.sh
+++ initial_test/create_diffs.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+diffsum ()
+{
+	find diffs -type f -name "$1" -print0 | xargs -0 ls -l | awk '{x+=$5} END {print "sum for pattern '$1': " x}';
+}
+
 N=5
 for ((i=1;i<=$N;i++)); do
 	for ((j=$i+1;j<=$N;j++)); do
 		echo "diff $i --> $j"
 		diff --minimal "$i.json" "$j.json" > "diffs/${i}_${j}.diff"
+		bsdiff "$i.json" "$j.json" "diffs/${i}_${j}.bsdiff"
+		vcdiff encode -dictionary "$i.json" < "$j.json" > "diffs/${i}_${j}.vcdiff"
 	done
 done
+
+diffsum '*.diff'
+diffsum '*.bsdiff'
+diffsum '*.vcdiff'
